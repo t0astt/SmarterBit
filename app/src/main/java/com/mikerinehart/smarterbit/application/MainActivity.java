fc227aeca71c60848cfbfaf12243a322893bcd64
mike <t0astt93@gmail.com>
2015-08-04 19:32:50 -0500

    Add XPosed Framework

--- app/src/main/java/com/mikerinehart/smarterbit/application/MainActivity.java
+++ app/src/main/java/com/mikerinehart/smarterbit/application/MainActivity.java
@@ -1,9 +1,11 @@
-package com.mikerinehart.smarterbit;
+package com.mikerinehart.smarterbit.application;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.mikerinehart.smarterbit.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/mikerinehart/smarterbit/xposed/SmarterBit.java
@@ -0,0 +1,14 @@
+package com.mikerinehart.smarterbit.xposed;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+/**
+ * Created by mike on 8/4/15.
+ */
+public class SmarterBit implements IXposedHookLoadPackage{
+
+    public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+
+    }
+}

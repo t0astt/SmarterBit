f25b51b87ff15d1a627f941031943c1bb8cdd2d7
mike <t0astt93@gmail.com>
2015-08-05 14:49:28 -0500

    Add SMS Notification Preferences

--- app/src/main/java/com/mikerinehart/smarterbit/application/fragments/SMSFragment.java
+++ app/src/main/java/com/mikerinehart/smarterbit/application/fragments/SMSFragment.java
@@ -4,21 +4,21 @@ import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.preference.CheckBoxPreference;
+import android.preference.PreferenceFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 
 import com.mikerinehart.smarterbit.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link SMSFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link SMSFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class SMSFragment extends Fragment {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class SMSFragment extends PreferenceFragment {
+
+    CheckBoxPreference mCheckBoxPreference;
 
     private OnFragmentInteractionListener mListener;
 
@@ -34,14 +34,16 @@ public class SMSFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.sms_preferences);
+        mCheckBoxPreference = (CheckBoxPreference)findPreference("smsEnabled");
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sms, container, false);
-    }
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_sms, container, false);
+//    }
 
     @Override
     public void onAttach(Activity activity) {

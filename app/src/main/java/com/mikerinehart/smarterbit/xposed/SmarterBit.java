524e6cba8bc63096089abe5422275d9353220eb5
mike <t0astt93@gmail.com>
2015-08-06 15:25:21 -0500

    Revise SMS Preferences XML, add option for notify on silent

--- app/src/main/java/com/mikerinehart/smarterbit/xposed/SmarterBit.java
+++ app/src/main/java/com/mikerinehart/smarterbit/xposed/SmarterBit.java
@@ -3,9 +3,6 @@ package com.mikerinehart.smarterbit.xposed;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
-/**
- * Created by mike on 8/4/15.
- */
 public class SmarterBit implements IXposedHookLoadPackage{
 
     public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {

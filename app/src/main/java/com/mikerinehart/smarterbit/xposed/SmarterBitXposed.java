a753bf0503b9431a2e1001a3c429a28df629a360
mike <t0astt93@gmail.com>
2015-08-16 13:45:20 -0500

    Prepare for publish

--- app/src/main/java/com/mikerinehart/smarterbit/xposed/SmarterBitXposed.java
+++ app/src/main/java/com/mikerinehart/smarterbit/xposed/SmarterBitXposed.java
@@ -6,13 +6,18 @@ import com.mikerinehart.smarterbit.xposed.enums.PackageName;
 import com.mikerinehart.smarterbit.xposed.hooks.Call;
 import com.mikerinehart.smarterbit.xposed.hooks.SMS;
 
+import de.robv.android.xposed.IXposedHookInitPackageResources;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.IXposedHookZygoteInit;
 import de.robv.android.xposed.XSharedPreferences;
 import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.callbacks.XC_InitPackageResources;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
-public class SmarterBitXposed implements IXposedHookLoadPackage, IXposedHookZygoteInit, SharedPreferences.OnSharedPreferenceChangeListener {
+public class SmarterBitXposed implements IXposedHookLoadPackage,
+        IXposedHookZygoteInit,
+        IXposedHookInitPackageResources,
+        SharedPreferences.OnSharedPreferenceChangeListener {
 
     private static XSharedPreferences prefs;
 
@@ -27,6 +32,12 @@ public class SmarterBitXposed implements IXposedHookLoadPackage, IXposedHookZygo
             SMS.initHooks(lpparam);
             Call.initHooks(lpparam);
             prefs.reload();
+        }
+    }
+
+    public void handleInitPackageResources(XC_InitPackageResources.InitPackageResourcesParam resparam) {
+        if (resparam.packageName.equals(PackageName.FITBIT.getPackageName())) {
+            resparam.res.setReplacement(PackageName.FITBIT.getPackageName(), "string", "call_notifications", "Enable SmarterBit");
         }
     }
 
